92bf4800a69977f9e0ef0d2e2ff17f4f0f346150
Cleibson Gomes da Silva <cleibson.gomes.silva@gmail.com>
2023-09-12 12:56:36 -0400

    Firs commit -> Start project Kotlin

---
+++ src/test/kotlin/com/nosbielc/kotlinspringapi/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nosbielc.kotlinspringapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
